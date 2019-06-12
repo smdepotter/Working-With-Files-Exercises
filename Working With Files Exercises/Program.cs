fc3d8cd2fd2f002913410814bb58daf7b4394ccf
Sean DePotter <seandep@live.com>
2019-06-12 03:36:21 -0400

    Add project files.

---
+++ Working With Files Exercises/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Working_With_Files_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise1();
+        }
+
+        static void Exercise1()
+        {
+            //Write a program that reads a text file and displays the number of words.
+            var path = @"C:\test\hellodoc.txt";
+
+            var contents = File.ReadAllText(path).Split(" ");
+            Console.WriteLine("You have this many words in {0}: {1} ", path, contents.Count());
+        }
+    }
+}
