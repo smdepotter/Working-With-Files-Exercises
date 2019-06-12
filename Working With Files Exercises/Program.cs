2acb4e256855c2aeec636dc6e56823618f56082a
Sean DePotter <seandep@live.com>
2019-06-12 03:45:42 -0400

    Exercise 2

--- Working With Files Exercises/Program.cs
+++ Working With Files Exercises/Program.cs
@@ -8,7 +8,7 @@ namespace Working_With_Files_Exercises
     {
         static void Main(string[] args)
         {
-            Exercise1();
+            Exercise2();
         }
 
         static void Exercise1()
@@ -18,6 +18,18 @@ namespace Working_With_Files_Exercises
 
             var contents = File.ReadAllText(path).Split(" ");
             Console.WriteLine("You have this many words in {0}: {1} ", path, contents.Count());
+        }
+
+        static void Exercise2()
+        {
+            //Write a program that reads a text file and displays the longest word in the file. 
+            var path = @"C:\test\hellodoc.txt";
+
+            var contents = File.ReadAllText(path).Split(" ");
+
+            var sorted = contents.OrderBy(n => n.Length);
+
+            Console.WriteLine("Your shortest word is: " + sorted.LastOrDefault());
         }
     }
 }
